e20ffd79ee2ae0e63efd65c32293e9d1903f60db
vpotturu777 <90070166+vpotturu777@users.noreply.github.com>
2022-03-25 16:23:15 -0400

    commit

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -9,10 +9,18 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView flashcardQuestion;
     TextView flashcardAnswer;
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +29,44 @@ public class MainActivity extends AppCompatActivity {
 
         flashcardQuestion = findViewById(R.id.flashcard_question_textview);
         flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question_textview)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer_textview)).setText(allFlashcards.get(0).getAnswer());
+        }
+
+        findViewById(R.id.flashcard_next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFlashcards.size() == 0)
+                    return;
+
+                currentCardDisplayedIndex++;
+
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Snackbar.make(v,
+                            "You've reached the end of the cards, going back to start.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                    currentCardDisplayedIndex = 0;
+                }
+
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+
+                ((TextView) findViewById(R.id.flashcard_question_textview)).setText(flashcard.getAnswer());
+                ((TextView) findViewById(R.id.flashcard_answer_textview)).setText(flashcard.getQuestion());
+            }
+        });
+
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flashcardQuestion.setVisibility(View.INVISIBLE);
-                flashcardAnswer.setVisibility(View.VISIBLE);
+                ((TextView) findViewById(R.id.flashcard_question_textview)).setVisibility(View.INVISIBLE);
+                ((TextView) findViewById(R.id.flashcard_answer_textview)).setVisibility(View.VISIBLE);
                 Toast.makeText(MainActivity.this, "I CLICKED THIS!", Toast.LENGTH_LONG).show();
             }
         });
@@ -47,6 +88,9 @@ public class MainActivity extends AppCompatActivity {
             String answer = data.getExtras().getString("answer");
             flashcardQuestion.setText(question);
             flashcardAnswer.setText(answer);
+
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
 }
